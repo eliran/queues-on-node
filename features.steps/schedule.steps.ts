b6894a4b0e8009c7d7d5453d3637b57447162fc8
Eliran Ben Ezra <eliran.ben-ezra@gamechanger.io>
2019-10-08 14:19:27 -0400

    initial job/queue feature passing

--- features.steps/schedule.steps.ts
+++ features.steps/schedule.steps.ts
@@ -1,44 +1,130 @@
 import { expect } from 'chai';
-import { Given, Then, When } from 'cucumber';
+import {After, Before, Given, Then, When} from 'cucumber';
+import * as Sinon from 'sinon';
+
+class Job {
+  public static make(): Job {
+    return new Job();
+  }
+
+  private executed = false;
+  private constructor() {}
+
+  get isExecuted(): boolean {
+    return this.executed;
+  }
+
+  public async execute(): Promise<void> {
+    this.executed = true;
+  }
+}
+
+interface ScheduleOptions {
+  job: Job;
+  after?: Date;
+}
+
+class Queue {
+  public static make(): Queue {
+    return new Queue();
+  }
+
+  private scheduled: ScheduleOptions[] = [];
+  private constructor() {
+    setInterval(this.processQueue.bind(this), 1000);
+  }
+
+  public schedule(options: ScheduleOptions) {
+    if (options.after) {
+      this.scheduled.push(options);
+    } else {
+      this.executeJob(options.job);
+    }
+  }
+
+  public cancel(job: Job) {
+    this.scheduled = this.scheduled.filter((options) => options.job !== job);
+  }
+
+  public isScheduled(job: Job): boolean {
+    for (const scheduledJob of this.scheduled) {
+      if (scheduledJob.job === job) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private executeJob(job: Job) {
+    job.execute().then(() => {}, () => {});
+  }
+
+  private processQueue() {
+    const now = new Date().getTime();
+    for (let i = this.scheduled.length - 1; i >= 0; i--) {
+      const options = this.scheduled[i];
+      if (!options.after || (now >= options.after.getTime())) {
+        this.scheduled.splice(i, 1);
+        this.executeJob(options.job);
+      }
+    }
+  }
+}
+
+Before(function() {
+  this.fakeTimers = Sinon.useFakeTimers();
+});
+
+After(function() {
+  this.fakeTimers.restore();
+});
 
 Given('I have a job', function() {
-  return 'pending';
+  this.job = Job.make();
 });
 
-When('I schedule it on a queue', function () {
-  return 'pending';
+Given('I have a queue', function() {
+  this.queue = Queue.make();
 });
 
-Then('the job should not execute by a worker', function() {
-  return 'pending';
+When('I schedule it on a queue', function() {
+  this.queue.schedule({ job: this.job });
 });
 
-Then('the job should execute by a worker', function() {
-  return 'pending';
+Then('the job should not execute', function() {
+  expect(this.job.isExecuted).to.be.false;
 });
 
+Then('the job should execute', function() {
+  expect(this.job.isExecuted).to.be.true;
+});
 
 Then('the job should be scheduled', function() {
-  return 'pending';
+  expect(this.queue.isScheduled(this.job)).to.be.true;
 });
 
-Then('the job should not be scheduled', function () {
-  return 'pending';
+Then('the job should not be scheduled', function() {
+  expect(this.queue.isScheduled(this.job)).to.be.false;
 });
 
-When('{int} hour elapsed', function (hours) {
-  return 'pending';
+When('{int} hour elapsed', function(hours) {
+  this.fakeTimers.tick(hours * 60 * 60 * 1000);
 });
 
-When('I schedule it on a queue in {int} hour', function (hours) {
-  return 'pending';
+When('{int} minutes elapsed', function(minutes) {
+  this.fakeTimers.tick(minutes * 60 * 1000);
 });
 
-When('{int} minutes elapsed', function (minutes) {
-  return 'pending';
+When('I schedule it on a queue in {int} hour', function(hours) {
+  this.queue.schedule({ job: this.job, after: relativeDate(hours * 60) });
 });
 
-When('I cancel the job', function () {
-  return 'pending';
+When('I cancel the job', function() {
+  this.queue.cancel(this.job);
 });
 
+function relativeDate(minutes: number): Date {
+  const date = new Date();
+  date.setTime(date.getTime() + minutes * 60 * 1000);
+  return date;
+}
--- features/schedule.feature
+++ features/schedule.feature
@@ -1,28 +1,29 @@
 Feature: Schedule Job API
   Background:
     Given I have a job
+    And I have a queue
 
   Scenario: Schedule a job immediately
     When I schedule it on a queue
-    Then the job should execute by a worker
+    Then the job should execute
     And the job should not be scheduled
 
   Scenario: Schedule a job in the future
     When I schedule it on a queue in 1 hour
     And 1 hour elapsed
-    Then the job should execute by a worker
+    Then the job should execute
     And the job should not be scheduled
 
   Scenario: Schedule a job in the future but time not passed yet
     When I schedule it on a queue in 1 hour
     And 59 minutes elapsed
     Then the job should be scheduled
-    And the job should not execute by a worker
+    And the job should not execute
 
   Scenario: Cancel a scheduled job
     When I schedule it on a queue in 1 hour
     And I cancel the job
     And 1 hour elapsed
     Then the job should not be scheduled
-    And the job should not execute by a worker
+    And the job should not execute
      
